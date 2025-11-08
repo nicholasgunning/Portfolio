8cfc7a7edfd6fe6f40271782e9bd6bb053f772a0
Nick Gunning <100513856+nicholasgunning@users.noreply.github.com>
2025-11-08 14:09:12 +1100

    fixed links

--- src/components/OverviewLinks.jsx
+++ src/components/OverviewLinks.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import "../stylesheets/Overview.scss";
+import resumePdf from "../assets/homePage/Resume.pdf";
 
 function OverviewLinks() {
-  const handleDownload = () => {};
-  const openLinkedIn = () => {
-    window.open("www.linkedin.com/in/nick-gunning-7189371b9/");
+  const handleDownload = () => {
+    "../assets/homePage/Resume.pdf";
   };
+
+  const openLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/nick-gunning-7189371b9/");
+  };
+
   return (
     <div className="overview-links">
-      <div className="overview-links-item" onClick={handleDownload}>
-        <div>01</div>
-        <div>Resume</div>
-      </div>
-      <div className="overview-links-item" onClick={openLinkedIn}>
-        <div>02</div>
-        <div>LinkedIn</div>
-      </div>
+      <a href={resumePdf} download="Nick_Gunning_Resume.pdf">
+        <div className="overview-links-item" onClick={handleDownload}>
+          <div>01</div>
+          <div>Resume</div>
+        </div>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nick-gunning-7189371b9/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="overview-links-item">
+          <div>02</div>
+          <div>LinkedIn</div>
+        </div>
+      </a>
     </div>
   );
 }
